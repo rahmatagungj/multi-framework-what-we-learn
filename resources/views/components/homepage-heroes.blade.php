d0c0ea152808ecc34b44e3ad75149aacc6924210
Rahmat Agung Julians <rahmatagungj@Gmail.com>
2021-09-05 09:57:15 +0700

    split heroes to component

---
+++ resources/views/components/homepage-heroes.blade.php
@@ -0,0 +1,28 @@
+<section>
+    <div class="flex flex-col px-5 py-16 mx-auto lg:items-center md:flex-row lg:px-28">
+        <div class="w-full mb-10 lg:w-5/6 lg:max-w-lg md:w-1/2">
+            <img class="object-cover object-center rounded" alt="hero" loading="lazy"
+                src="https://skksooriarachchi.com/wp-content/uploads/2019/05/online-learning.png">
+        </div>
+        <div class="flex flex-col items-start text-left lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16">
+            <h2 class="mb-8 text-2xl font-bold tracking-tighter text-left text-black lg:text-5xl title-font"> Share
+                Your Knowledge</h2>
+            <p class="mb-8 text-base leading-relaxed text-left ">Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Voluptates facere numquam vero eveniet! Facilis, vero sapiente, nisi maiores inventore soluta
+                eveniet voluptatum magnam quod debitis recusandae aliquid unde?</p>
+            <div class="flex flex-col justify-left lg:flex-row">
+                <a href="submit">
+                    <button
+                        class="flex items-center px-6 py-2 mt-auto font-semibold text-white bg-gray-800 rounded hover:bg-gray-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                        Show me </button>
+                </a>
+                <p class="mt-2 text-sm text-left text-blueGray-600 md:ml-6 md:mt-0"> It will take you to popular. <br
+                        class="hidden lg:block">
+                    <a href="#"
+                        class="inline-flex items-center font-semibold text-blue-600 md:mb-2 lg:mb-0 hover:text-black "
+                        title="read more"> Read more about it » </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
